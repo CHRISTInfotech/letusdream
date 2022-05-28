549b84fba5e53db7473f892b394050a2ee07019c
rohanhar16 <harchandani586@gmail.com>
2022-05-28 18:22:45 +0530

    create views and urls for home

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import index
+from home.views import index, locations, aboutUs, media, aboutConference, trriennialConference, buNY, cuB, cuT, jncaB, \
+    ltuLouisiana, dreams
 
-urlpatterns = [
+urlpatterns = {
     path('', index, name='main'),
-]
+    path('location/<str:loc>', locations, name='locations'),
+    path('about-us', aboutUs, name='about-us'),
+    path('media', media, name='media'),
+    path('about-conference', aboutConference, name='about-conference'),
+    path('annual-conference', annualConference, name='annual-conference'),
+    path('trriennial-conference', trriennialConference, name='trriennial-conference'),
+    path('binghamton-university-new-york', buNY, name='binghamton-university-new-york'),
+    path('christ-university-bangalore', cuB, name = 'christ-university-bangalore'),
+    path('christ-university-thiruvananthapuram', cuT, name = 'christ-university-thiruvananthapuram'),
+    path('jyoti-nivas-college-autonomous', jncaB, name = 'jyoti-nivas-college-autonomous'),
+    path('louisiana-tech-university', ltuLouisiana, name = 'louisiana-tech-university'),
+    path('dreams/<str:drm>', dreams, name='dreams')
+}
--- research/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from research.views import publications, research
-
-urlpatterns = [
-    path('', research, name='research'),
-    path('publications', publications, name='publications'),
-]
