20649a0e5d64af0771ef985b8e7da54b90b81872
rohanhar16 <harchandani586@gmail.com>
2022-06-04 09:20:13 +0530

    updates in index pages

--- home/urls.py
+++ home/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from home.views import index, locations, aboutUs, media, aboutConference, trriennialConference, buNY, cuB, cuT, jncaB, \
-    ltuLouisiana, dreams
+    ltuLouisiana, dreams, annualConference
 
 urlpatterns = {
     path('', index, name='main'),
     path('location/<str:loc>', locations, name='locations'),
     path('about-us', aboutUs, name='about-us'),
-    path('media', media, name='media'),
+    path('ltmedia', media, name='ltmedia'),
     path('about-conference', aboutConference, name='about-conference'),
     path('annual-conference', annualConference, name='annual-conference'),
     path('trriennial-conference', trriennialConference, name='trriennial-conference'),
@@ -17,5 +17,5 @@ urlpatterns = {
     path('christ-university-thiruvananthapuram', cuT, name = 'christ-university-thiruvananthapuram'),
     path('jyoti-nivas-college-autonomous', jncaB, name = 'jyoti-nivas-college-autonomous'),
     path('louisiana-tech-university', ltuLouisiana, name = 'louisiana-tech-university'),
-    path('dreams/<str:drm>', dreams, name='dreams')
+    path('dreams/<str:drm>', dreams, name='dreams'),
 }
