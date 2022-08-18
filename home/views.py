f401c0f3be187a8af8cd4512c7810e95562f3e20
alanso-mathew <alanso.mathew@christuniversity.in>
2022-08-18 16:11:24 +0530

    Changes in the Photo gallery section and change the url

--- home/views.py
+++ home/views.py
@@ -13,11 +13,13 @@ def aboutUs(request):
 def media(request):
     return render(request, 'aboutUs/media.html')
 
-def mediaPhotos(request):
-    return render(request,'aboutUs/media-view-photo.html')
+
+def pressRelease(request):
+    return render(request, 'aboutUs/media-view-photo.html')
+
 
 def photoGallery(request):
-    return render(request,'aboutUs/photo-gallery.html')
+    return render(request, 'aboutUs/photo-gallery.html')
 
 
 def aboutConference(request):
