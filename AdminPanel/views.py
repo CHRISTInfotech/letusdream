5f4fc584d49a6069e73492ff14915722e44d64db
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-09-03 15:55:16 +0530

    Admin folder is added

---
+++ AdminPanel/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def signup(request):
+    return render(request, 'Admin/login.html')
+
+def verification(request):
+    return render(request,"Admin/otp-verification.html")
+
+def dashboard(request):
+    return render(request,"Admin/home.html")
+
+def registration(request):
+    return render(request,"Admin/registration.html")
+
+def view_dt(request):
+    return render(request,"Admin/view-dt.html")
