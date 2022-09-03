d1c2b16a1854d6f5744b6a3f44c9d692ab2ab7b5
alanso-mathew <alanso.mathew@christuniversity.in>
2022-09-03 12:38:00 +0530

    change all the style into external css

--- ltstatic/js/main.js
+++ ltstatic/js/main.js
@@ -1,2 +1,10 @@
 
-    
+     $(window).on('scroll', function () {
+
+        if ($(window).scrollTop()) {
+            $('#navbar').addClass('black');
+        }
+        else {
+            $('#navbar').removeClass('black');
+        }
+    })
