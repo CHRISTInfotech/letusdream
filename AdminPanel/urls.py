c35966bfdf957c95b1ea68e26bde273a7e40cde1
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-09-27 16:48:03 +0530

    Louisiana tech university page is updated

--- AdminPanel/urls.py
+++ AdminPanel/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from AdminPanel.views import  dashboard, events, modify_statistics, registration, reports, signup, verification, view_dt
+from AdminPanel.views import  dashboard, events, modify_statistics, registration, reports, signup, view_dt
 
 
 urlpatterns = [
     path('signup',signup,name="signup"),
-    path('verification',verification,name="verification"),
+    # path('verification',verification,name="verification"),
     path('dashboard',dashboard,name="dashboard"),
     path('registration',registration,name="registration"),
     path('view_dt',view_dt,name="view-dt"),
