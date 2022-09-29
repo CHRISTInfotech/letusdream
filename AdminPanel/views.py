7a4d0f1ef304134bb74f36144fc86206b019d4d8
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-09-29 09:24:15 +0530

    Registration page is modified according to the requirement

--- AdminPanel/views.py
+++ AdminPanel/views.py
@@ -2,6 +2,8 @@ import statistics
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 
+from AdminPanel.models import Registration
+
 # Create your views here.
 
 # def verification(request):
@@ -13,7 +15,27 @@ def dashboard(request):
 
 
 def registration(request):
-    return render(request, "AdminPanel/registration.html")
+
+    query_results = Registration.objects.all()
+    print(query_results)
+    
+
+    if request.method == "POST":
+        fname=request.POST.get("fname")
+        lname=request.POST.get('lname')
+        phone=request.POST.get('phone')
+        email=request.POST.get('email')
+        dreams=request.POST.get('dreams')
+        dreamsOption=request.POST.getlist('dreamsOption')
+        # print(dreamsOption)
+        en=Registration.objects.create(fname=fname,lname=lname,phone=phone,email=email,dreams=dreams,dreamsOption=dreamsOption)
+        # print(en)
+        en.save()
+        
+        return render(request,"AdminPanel/registration.html",{'query_results':query_results})
+        # return redirect("registration")
+    else:
+        return render(request, "AdminPanel/registration.html",{'query_results':query_results})
 
 
 def view_dt(request):
@@ -37,16 +59,23 @@ def modify_statistics(request):
 
 def signup(request):
     if request.method == "POST":
-       username=request.POST['username']
-       password=request.POST['password']
+        username=request.POST['username']
+        password=request.POST['password']
 
-       user=authenticate(request,username=username,password=password)
+        user=authenticate(request,username=username,password=password)
       
-       if user is not None:
-        login(request,user)
-        # return HttpResponseRedirect('/AdminPanel/verification/')
-        return redirect("dashboard")
+        if user is not None:
+            login(request,user)
+            # return HttpResponseRedirect('/AdminPanel/verification/')
+            return redirect("dashboard")
+
+        else:
+            return redirect("signup")
+
 
     else :
         return render(request,'AdminPanel/login.html')
+
+
+        
 
