1366dd303908ba1ae2a60db8021a9d612456c608
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-09-29 09:24:15 +0530

    Registration page is modified according to the requirement

--- AdminPanel/models.py
+++ AdminPanel/models.py
@@ -1,3 +1,22 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
+
 # Create your models here.
+class Registration(models.Model):
+    fname=models.CharField(max_length=50)
+    lname=models.CharField(max_length=50)
+    phone=models.CharField(max_length=20,null=True)
+    email=models.CharField(max_length=50)
+    dreams=models.CharField(max_length=20)
+    dreamsOption=models.CharField(max_length=20)
+
+
+# class DreamsEvent(models.Model):
+#     events=models.CharField(max_length=20)
+
+# class DreamsOption(models.Model):
+#     options=models.CharField(max_length=20)
+#     events=models.ForeignKey(DreamsEvent,ondelete=models.CASCADE)
+
+    
