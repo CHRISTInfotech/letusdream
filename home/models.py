cb0aec2540c7b42aa0133e6f6563694e4128ac82
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-10-12 16:33:28 +0530

    In Registration Page Validation is applied and data is storing in database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ConferenceRegistration(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=20)
+    aboutme=models.CharField(max_length=20)
+    aboutother=models.CharField(max_length=100,null=True)
+    orgaff=models.CharField(max_length=50)
+    orgname=models.CharField(max_length=50)
+    phone=models.CharField(max_length=20,null=True)
+    confdate=models.CharField(max_length=50)
+    prevconf=models.CharField(max_length=50)
+
