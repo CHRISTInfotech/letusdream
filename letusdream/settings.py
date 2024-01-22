2bfd779d2cf178523eedc180fdf242f755ee1c9a
alwin-97 <alwinjosephkaduppil@gmail.com>
2024-01-22 11:11:17 +0530

    adding club pages

--- letusdream/settings.py
+++ letusdream/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-)ij8yr40t__zu2z_$ve9@_#7x7183^v-ki9b(0i_7l60ndhz6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if DEBUG == False:
+if not DEBUG:
     ALLOWED_HOSTS = ['10.5.5.31', 'letusdream.org']
 else:
     ALLOWED_HOSTS = ['*']
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'letusdream.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -116,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
